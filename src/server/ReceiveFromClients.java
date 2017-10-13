940a08765d270d287b1ad0176b3cd5edac834c61
Saulo de Sousa <saulos.joseph@gmail.com>
2017-10-13 01:04:56 -0300

    implemented client chat

--- src/server/ReceiveFromClients.java
+++ src/server/ReceiveFromClients.java
@@ -7,15 +7,18 @@ import java.util.Scanner;
 class ReceiveFromClients implements Runnable {
 
 	private Socket c;
-
-	public ReceiveFromClients(Socket client) {
+	private Server s;
+	
+	public ReceiveFromClients(Socket client, Server server) {
 		this.c = client;
+		this.s = server;
 	}
 
 	public void run() {
 		try(Scanner input = new Scanner(this.c.getInputStream())){
 			while (input.hasNextLine()) {
-				System.out.println(this.c.getInetAddress().getHostAddress() + ": " + input.nextLine());
+				//System.out.println(this.c.getInetAddress().getHostAddress() + ": " + input.nextLine());
+				s.sendToClients(c, this.c.getInetAddress().getHostAddress() + ": " + input.nextLine());
 			}
 			System.out.println(this.c.getInetAddress().getHostAddress() + " Desconected.");
 			input.close();
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,15 +1,20 @@
 package server;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
 	
 	private int port;
+	private List<Socket> clients;
 	
 	public Server(int porta) {
 		this.port = porta;
+		this.clients = new ArrayList<>();
 	}
 	
 	public void run() throws IOException {
@@ -17,10 +22,20 @@ public class Server {
 			System.out.println("Waiting...");			
 			while(true) {
 				Socket client = server.accept();
+				clients.add(client);
 				System.out.println(client.getInetAddress().getHostAddress() + " Conected.");
-				ReceiveFromClients receive = new ReceiveFromClients(client);
+				ReceiveFromClients receive = new ReceiveFromClients(client, this);
 				new Thread(receive).start();
 			}			
+		}
+	}
+	
+	public void sendToClients(Socket client, String msg) throws IOException {
+		for(Socket c : clients) {
+			if(!c.equals(client)){
+				PrintStream ps = new PrintStream(c.getOutputStream());
+				ps.println(msg);
+			}
 		}
 	}
 	
